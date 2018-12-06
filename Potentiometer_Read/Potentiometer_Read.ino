dca6f11aedb63e341b210b0c856a24c0b500472b
JoshuaD9907 <45548505+JoshuaD9907@users.noreply.github.com>
2018-12-05 22:24:06 -0700

    Update Potentiometer_Read.ino
    Yellow and Green Buttons Functional.

--- Potentiometer_Read/Potentiometer_Read.ino
+++ Potentiometer_Read/Potentiometer_Read.ino
@@ -2,7 +2,9 @@ unsigned long time;
 int buttonStateYellow = 0; //detect button state
 int ledPinYellow = 5;
 int buttonStateGreen = 0;
+int ledPinGreen = 6;
 int buttonStateRed = 0;
+int buttonStateBlack = 0;
 bool buttonReady = false; //if the game is running
 int ledStartTime;
 int reactionTime;
@@ -16,6 +18,7 @@ int difficultyTime=0;
 
 void setup() {
   pinMode(ledPinYellow,OUTPUT); //set inputs and outputs
+  pinMode(ledPinGreen,OUTPUT); 
   pinMode(2,INPUT);
   pinMode(3,INPUT);
   pinMode(4,INPUT);
@@ -27,6 +30,7 @@ void loop() {
   buttonStateYellow = digitalRead(2); //set button pins
   buttonStateGreen = digitalRead(3);
   buttonStateRed = digitalRead(4);
+  buttonStateBlack = digitalRead(6);
   potVal = analogRead(potPin);    // read the value from the sensor
   potVal = (potVal*(1.00/1023)); //modify potVal to be 0-1
   
@@ -35,37 +39,67 @@ void loop() {
       if(abs((potPrev - potVal)*100) > 1){ //if difficulty changed more than 1%
          Serial.println((String)"Difficulty: " + (int)(potVal*100) + "%"); //print difficulty
          Serial.println("Press red to start");
-         gameStart=false; //stop game if difficulty is changed
-         digitalWrite(ledPinYellow, LOW); //turn off LED
         }
        difficultyTime = time;
        potPrev = potVal;
     }
-
 if(buttonStateRed==1){ //start game
   gameStart=true;
+  delay(100);
 }
+
 
 if(gameStart==true){ //if game has been started, run this
     if(buttonReady==false){ //if the game is not already running
       if(buttonStateYellow==0){
-        delay(random(300,3000)*(2-(potVal+.5))); //set timer
-        delay(100);
-        digitalWrite(ledPinYellow, HIGH);   // turn on LED
-        Serial.println("Start");
-        time = millis();
-        ledStartTime = time;
-        buttonReady=true; 
+        if(buttonStateGreen==0){
+          if(buttonStateRed==0){
+            if(random(0,2)==0){
+              delay(random(300,3000)*(2-(potVal+.5))); //set timer
+              delay(100);
+              digitalWrite(ledPinYellow, HIGH);   // turn on LED
+              Serial.println("Start");
+              time = millis();
+              ledStartTime = time;
+              buttonReady=true; 
+            }else{
+              delay(random(300,3000)*(2-(potVal+.5))); //set timer
+              delay(100);
+              digitalWrite(ledPinGreen, HIGH);   // turn on LED
+              Serial.println("Start");
+              time = millis();
+              ledStartTime = time;
+              buttonReady=true; 
+            }
+          }
+        }
       }
     }
-
   if(buttonReady == true){
+    if(buttonStateRed==1){ //end game
+      gameStart=false;
+      buttonReady=false;
+      digitalWrite(ledPinYellow, LOW); //turn off LED
+      digitalWrite(ledPinGreen, LOW); //turn off LED
+      Serial.println("End Round");
+      potPrev = -1;
+      delay(100);
+    }
     if(buttonStateYellow==1){
       Serial.print("Time: ");
       time = millis();
       reactionTime = time-ledStartTime;
       Serial.println(reactionTime);    //prints time since program started       
       digitalWrite(ledPinYellow, LOW);
+      buttonReady=false;
+      delay(100); 
+    }
+    if(buttonStateGreen==1){
+      Serial.print("Time: ");
+      time = millis();
+      reactionTime = time-ledStartTime;
+      Serial.println(reactionTime);    //prints time since program started       
+      digitalWrite(ledPinGreen, LOW);
       buttonReady=false;
       delay(100); 
     }
