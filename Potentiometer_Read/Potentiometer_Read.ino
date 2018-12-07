ce4875ff0bbb0b8ea5e89d236613ef2c453d08c4
JoshuaD9907 <45548505+JoshuaD9907@users.noreply.github.com>
2018-12-06 22:28:08 -0700

    Update Potentiometer_Read.ino
    updated code

--- Potentiometer_Read/Potentiometer_Read.ino
+++ Potentiometer_Read/Potentiometer_Read.ino
@@ -1,11 +1,14 @@
+#define ledPinYellow 5
+#define ledPinGreen 6
+#define ledPinRed 7
+#define butPinRed 4
+#define butPinGreen 3
+#define butPinYellow 2
 unsigned long time;
 int buttonStateYellow = 0; //detect button state
-int ledPinYellow = 5;
 int buttonStateGreen = 0;
-int ledPinGreen = 6;
 int buttonStateRed = 0;
-int ledPinRed = 7;
-int buttonStateBlack = 0;
+int currentButton = 0;
 bool buttonReady = false; //if the game is running
 int ledStartTime;
 int reactionTime;
@@ -20,18 +23,18 @@ int difficultyTime=0;
 void setup() {
   pinMode(ledPinYellow,OUTPUT); //set inputs and outputs
   pinMode(ledPinGreen,OUTPUT); 
-  pinMode(ledPinRed,OUTPUT); 
-  pinMode(2,INPUT_PULLUP);
-  pinMode(3,INPUT_PULLUP);
-  pinMode(4,INPUT_PULLUP);
+  pinMode(ledPinRed,OUTPUT);
+  pinMode(butPinRed,INPUT);
+  pinMode(butPinYellow,INPUT);
+  pinMode(butPinGreen,INPUT);
   pinMode(potPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  buttonStateYellow = digitalRead(2); //set button pins
-  buttonStateGreen = digitalRead(3);
-  buttonStateRed = digitalRead(4);
+  buttonStateYellow = digitalRead(butPinYellow); //set button pins
+  buttonStateGreen = digitalRead(butPinGreen);
+  buttonStateRed = digitalRead(butPinRed);
   potVal = analogRead(potPin);    // read the value from the sensor
   potVal = (potVal*(1.00/1023)); //modify potVal to be 0-1
   
@@ -46,27 +49,35 @@ void loop() {
     }
 if(buttonStateRed==1){ //start game
   gameStart=true;
-  delay(100);
+  delay(10);
 }
-
 
 if(gameStart==true){ //if game has been started, run this
     if(buttonReady==false){ //if the game is not already running
       if(buttonStateYellow==0){
         if(buttonStateGreen==0){
           if(buttonStateRed==0){
-            if(random(0,2)==0){
-              delay(random(300,3000)*(2-(potVal+.5))); //set timer
-              delay(100);
+            currentButton=random(0,3);
+            if(currentButton==0){
+              delay(random(300,1000)*(11-(10*potVal)+.5)); //set timer
+              delay(10);
               digitalWrite(ledPinYellow, HIGH);   // turn on LED
               Serial.println("Start");
               time = millis();
               ledStartTime = time;
               buttonReady=true; 
-            }else{
-              delay(random(300,3000)*(2-(potVal+.5))); //set timer
-              delay(100);
+            }else if(currentButton==1){
+              delay(random(300,1000)*(11-(10*potVal)+.5)); //set timer
+              delay(10);
               digitalWrite(ledPinGreen, HIGH);   // turn on LED
+              Serial.println("Start");
+              time = millis();
+              ledStartTime = time;
+              buttonReady=true; 
+            } else if(currentButton==2){
+              delay(random(300,1000)*(11-(10*potVal)+.5)); //set timer
+              delay(10);
+              digitalWrite(ledPinRed, HIGH);   // turn on LED
               Serial.println("Start");
               time = millis();
               ledStartTime = time;
@@ -77,32 +88,66 @@ if(gameStart==true){ //if game has been started, run this
       }
     }
   if(buttonReady == true){
-    if(buttonStateRed==1){ //end game
-      gameStart=false;
-      buttonReady=false;
-      digitalWrite(ledPinYellow, LOW); //turn off LED
-      digitalWrite(ledPinGreen, LOW); //turn off LED
-      Serial.println("End Round");
-      potPrev = -1;
-      delay(100);
-    }
+    
     if(buttonStateYellow==1){
-      Serial.print("Time: ");
-      time = millis();
-      reactionTime = time-ledStartTime;
-      Serial.println(reactionTime);    //prints time since program started       
-      digitalWrite(ledPinYellow, LOW);
-      buttonReady=false;
-      delay(100); 
+      if(currentButton==0){
+        Serial.print("Time: ");
+        time = millis();
+        reactionTime = time-ledStartTime;
+        Serial.println(reactionTime);    //prints time since program started       
+        digitalWrite(ledPinYellow, LOW);
+        buttonReady=false;
+        delay(100); 
+      }else{
+        gameStart=false;
+        buttonReady=false;
+        digitalWrite(ledPinYellow, LOW); //turn off LED
+        digitalWrite(ledPinGreen, LOW); //turn off LED
+        digitalWrite(ledPinRed, LOW); //turn off LED
+        Serial.println("End Round");
+        potPrev = -1;
+        delay(10);
+      }
     }
     if(buttonStateGreen==1){
-      Serial.print("Time: ");
-      time = millis();
-      reactionTime = time-ledStartTime;
-      Serial.println(reactionTime);    //prints time since program started       
-      digitalWrite(ledPinGreen, LOW);
-      buttonReady=false;
-      delay(100); 
+      if(currentButton==1){
+        Serial.print("Time: ");
+        time = millis();
+        reactionTime = time-ledStartTime;
+        Serial.println(reactionTime);    //prints time since program started       
+        digitalWrite(ledPinGreen, LOW);
+        buttonReady=false;
+        delay(100); 
+      }else{
+        gameStart=false;
+        buttonReady=false;
+        digitalWrite(ledPinYellow, LOW); //turn off LED
+        digitalWrite(ledPinGreen, LOW); //turn off LED
+        digitalWrite(ledPinRed, LOW); //turn off LED
+        Serial.println("End Round");
+        potPrev = -1;
+        delay(10);
+      }
+    }
+    if(buttonStateRed==1){
+      if(currentButton==2){
+        Serial.print("Time: ");
+        time = millis();
+        reactionTime = time-ledStartTime;
+        Serial.println(reactionTime);    //prints time since program started       
+        digitalWrite(ledPinRed, LOW);
+        buttonReady=false;
+        delay(100); 
+      }else{
+        gameStart=false;
+        buttonReady=false;
+        digitalWrite(ledPinYellow, LOW); //turn off LED
+        digitalWrite(ledPinGreen, LOW); //turn off LED
+        digitalWrite(ledPinRed, LOW); //turn off LED
+        Serial.println("End Round");
+        potPrev = -1;
+        delay(10);
+      }
     }
     //delay(10); 
   }
